1a5c4b8cf9b2667926d9b5764a428a000e328138
alifu <alif.bangets@gmail.com>
2025-10-20 15:53:08 +0700

    update command to quit app first before install

--- Casks/simdeeplink.rb
+++ Casks/simdeeplink.rb
@@ -1,6 +1,6 @@
 cask "simdeeplink" do
-  version "0.0.2"
-  sha256 "e68713ff99512c4ae7ba7ded510bc7ad3b7d4f65c77a20d2c15c97d0dd60c790"
+  version "0.0.3"
+  sha256 "ec84e2c491a311fd5914fcce1bf6b3cca024e0aede880359af68c8fc0be26507"
 
   url "https://github.com/alifu/SimDeeplink/releases/download/#{version}/SimDeeplink.zip"
   name "SimDeeplink"
@@ -9,13 +9,16 @@ cask "simdeeplink" do
 
   app "SimDeeplink.app"
 
-  zap trash: [
-    "~/Library/Preferences/AppWork.SimDeeplink.plist",
-    "~/Library/Application Support/SimDeeplink",
-  ]
+  uninstall quit: "AppWork.SimDeeplink",
+            delete: [
+              "/Applications/SimDeeplink.app",
+              "~/Applications/SimDeeplink.app",
+            ]
 
-  uninstall delete: [
+  zap trash: [
     "/Applications/SimDeeplink.app",
     "~/Applications/SimDeeplink.app",
+    "~/Library/Preferences/AppWork.SimDeeplink.plist",
+    "~/Library/Application Support/SimDeeplink",
   ]
 end

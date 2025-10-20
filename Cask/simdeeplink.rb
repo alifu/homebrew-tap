e21a046140b33290aec5fd575d94b30756146970
alifu <alif.bangets@gmail.com>
2025-10-20 13:17:54 +0700

    Fix SimDeeplink cask URL

--- Cask/simdeeplink.rb
+++ Cask/simdeeplink.rb
@@ -2,7 +2,7 @@ cask "simdeeplink" do
   version "0.0.1"
   sha256 "f6d4bcfd62a57ff112638236d847d43f72c9ff7dc6214ae28fedc994c5137b05"
 
-  url "https://github.com/alifu/SimDeeplink/releases/download/0.0.1/SimDeeplink.zip"
+  url "https://github.com/alifu/SimDeeplink/releases/download/#{version}/SimDeeplink.zip"
   name "SimDeeplink"
   desc "SimDeeplink (Deeplink Runner) is a desktop utility designed to make testing and running app deeplinks in Simulator effortless."
   homepage "https://github.com/alifu/SimDeeplink"
